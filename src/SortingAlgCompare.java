e69e8561049839910621d1305dfba318e78a35eb
Hannibal-ad-Portas <jconroy12@gmail.com>
2017-01-31 15:48:19 -0500

    New Repo

---
+++ src/SortingAlgCompare.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jconr on 1/31/2017.
+ */
+public class SortingAlgCompare {
+    public static void main (String[] args) {
+        System.out.println ("Hello. Are you an evil user?");
+        System.out.println("Y/N");
+
+    }
+}
